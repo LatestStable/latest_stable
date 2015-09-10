a17c4913f307023384c816752d3f43897e4ebc7d
Harald Wartig <hwartig@gmail.com>
2015-09-10 21:52:46 +0200

    Refactoring specs to be more high level.

--- features/step_definitions/bundler_steps.rb
+++ features/step_definitions/bundler_steps.rb
@@ -1,24 +1,91 @@
-Given(/^a Gemfile\.lock where "([^"]*)" is set to version "([^"]*)"$/) do |gem, version|
+Given(/^a bundle where all gems are up to date$/) do
+  step(
+    'a file named "Gemfile" with:',
+    <<-EOS.strip_heredoc
+    source 'https://rubygems.org'
+    gem 'papla'
+    gem 'minitest'
+    gem 'rake'
+    EOS
+  )
   step(
     'a file named "Gemfile.lock" with:',
     <<-EOS.strip_heredoc
     GEM
       remote: https://rubygems.org/
       specs:
-        #{gem} (#{version})
+        i18n (0.7.0)
         minitest (5.8.0)
+        papla (0.1.2)
+          i18n
         rake (10.4.2)
 
     PLATFORMS
       ruby
 
     DEPENDENCIES
-      #{gem}
+      i18n
       minitest
+      papla
       rake
 
     BUNDLED WITH
        1.10.6
     EOS
   )
+end
+
+Given(/^a bundle where a gem is out of date$/) do
+  step(
+    'a file named "Gemfile" with:',
+    <<-EOS.strip_heredoc
+    source 'https://rubygems.org'
+    gem 'papla'
+    gem 'minitest'
+    gem 'rake'
+    EOS
+  )
+  step(
+    'a file named "Gemfile.lock" with:',
+    <<-EOS.strip_heredoc
+    GEM
+      remote: https://rubygems.org/
+      specs:
+        i18n (0.7.0)
+        minitest (5.8.0)
+        papla (0.1.1)
+          i18n
+        rake (10.4.2)
+
+    PLATFORMS
+      ruby
+
+    DEPENDENCIES
+      i18n
+      minitest
+      papla
+      rake
+
+    BUNDLED WITH
+       1.10.6
+    EOS
+  )
+end
+
+Then(/^I should see that I'm already on latest stable$/) do
+  step('the output should contain "Already on latest stable"')
+end
+
+Then(/^then I should see which gems have been updated$/) do
+  step(
+    'the output should contain:',
+    <<-EOS.strip_heredoc
+    Updated 'papla' from 0.1.1 to 0.1.2
+    success
+    EOS
+  )
+end
+
+Then(/^all gems in the bundle should be up to date$/) do
+  step('the file "Gemfile.lock" should contain "papla (0.1.2)"')
 end
--- features/update_gem.feature
+++ features/update_gem.feature
@@ -4,25 +4,13 @@ Feature: Update gem to latest version
   I want to be able to run a single command to update all gems to newer
   versions where the update doesn't fail the tests
 
-  Background:
-    Given a file named "Gemfile" with:
-      """
-      source 'https://rubygems.org'
-      gem 'papla'
-      """
   Scenario: Gem is already up to date
-    Given a Gemfile.lock where "papla" is set to version "0.1.2"
+    Given a bundle where all gems are up to date
     When I run `latest_stable`
-    Then the output should contain "Already on latest stable"
+    Then I should see that I'm already on latest stable
 
   Scenario: Gem needs minor version update and tests pass with new version
-    Given a Gemfile.lock where "papla" is set to version "0.1.1"
+    Given a bundle where a gem is out of date
     When I run `latest_stable`
-    Then the output should contain:
-      """
-      Updated 'papla' from 0.1.1 to 0.1.2
-      Added 'i18n' 0.7.0
-      success
-      """
-    And the file "Gemfile.lock" should contain "papla (0.1.2)"
-    And the file "Gemfile.lock" should contain "i18n (0.7.0)"
+    Then then I should see which gems have been updated
+    And all gems in the bundle should be up to date
